4647a33170f64a184fb8f66098f01ff1e46da50e
zzaustc <zzaustc@163.com>
2016-11-30 23:16:30 +0800

    stage components

---
+++ StageXY/micro.h
@@ -0,0 +1,10 @@
+
+int InitMicroHandle();
+
+void MicroInfo(int handle);
+
+int GamePadXY(int direction,int handle);
+
+int MoveXY(double xposit,double yposit,int handle);
+
+void ReleaseMicroHandle(int handle);
